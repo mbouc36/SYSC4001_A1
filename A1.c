6912bb73a0562a39694d594f5149bdc5355a773f
NashM21 <manashaabhayaratne@cmail.carleton.ca>
2024-09-16 10:31:32 -0400

    Update A1.c

--- A1.c
+++ A1.c
@@ -2,3 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 
+int main(){
+    FILE  *file_ptr;
+
+    file_ptr = fopen("trace.txt","r");
+
+    char ch;
+
+    if (NULL == file_ptr){
+        printf("File can't open\n");
+        return 0;
+
+    }
+
+    while ((ch = fgetc(file_ptr)) != EOF){
+        printf("%c",ch);
+    }
+    fclose(file_ptr);
+    return 0;
+
+}
