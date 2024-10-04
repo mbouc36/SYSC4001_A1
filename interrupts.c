7d69e75efc3dc74dad9c00642e4e2e5c7734f0a8
mbouc36 <mbouc36@gmail.com>
2024-10-04 13:14:57 -0400

    added sleep

--- interrupts.c
+++ interrupts.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include "interrupts.h"
+#include <unistd.h>
 
 
 //switches user modes (1 ms)
 void switch_user_modes(int *current_time, FILE *execution_file){
+    usleep(1*1000);
     fprintf(execution_file, "%d, %d, switch to kernel mode\n", *current_time, 1);
     *current_time += 1; 
 }
@@ -15,21 +17,24 @@ void save_restore_context(int *current_time, FILE *execution_file){
     int min = 1;
     int max = 3;
 
-    // Generate a random number between 10 and 30
+    // Generate a random number between 1 and 3
     int randomInt = rand() % (max - min + 1) + min;
+    usleep(randomInt*1000);
     fprintf(execution_file, "%d, %d, context saved\n", *current_time, randomInt);
     *current_time += randomInt;
 }
 
 // calculates where in memory the ISR starts (1ms)
 void get_ISR_start_address(int *current_time, unsigned int vector_table_address, FILE *execution_file){
+    usleep(1*1000);
     fprintf(execution_file, "%d, %d, find vector %d in memory position 0x%04x\n", *current_time, 1, vector_table_address, vector_table_address * 2); // memeory position is vector number in hex + 0x2
     *current_time += 1; 
 }
 
 
-//get the address if the ISR from the vector table (1 ms + duration of ISR)
+//get the address if the ISR from the vector table (1 ms)
 void load_vector_address_to_pc(int *current_time, unsigned int ISR_address, FILE *execution_file){
+    usleep(1*1000);
     fprintf(execution_file, "%d, %d, load address 0x%04x into the PC\n", *current_time, 1, ISR_address);
     *current_time += 1; 
 }
@@ -37,6 +42,11 @@ void load_vector_address_to_pc(int *current_time, unsigned int ISR_address, FILE
 //get ISR start address, address from vector table, execute the instructions for the ISR (ISR execute time +1 +1)
 //This needs to be completelty redone
 void execute_ISR(int *current_time, int ISR_duration, FILE *execution_file){
+    if (ISR_duration > 1000){
+        sleep(ISR_duration/1000);
+    } else {
+        usleep(ISR_duration*1000);
+    }
 
     fprintf(execution_file, "%d, %d, SYSCALL: run the ISR\n", *current_time, ISR_duration * 50 / 100);
     *current_time += ISR_duration * 0.50;
@@ -49,6 +59,7 @@ void execute_ISR(int *current_time, int ISR_duration, FILE *execution_file){
 
 //Return the value from the interrupt subroutine ( 1ms)
 void IRET(int *current_time, FILE *execution_file){
+    usleep(1*1000);
     fprintf(execution_file, "%d, %d, IRET\n", *current_time, 1);
     *current_time += 1; 
 
@@ -57,21 +68,26 @@ void IRET(int *current_time, FILE *execution_file){
 
 //Handles the end of I/O situation
 void end_of_IO(int *current_time, int ISR_duration, FILE *execution_file){
-
+    if (ISR_duration > 1000){
+        sleep(ISR_duration/1000);
+    } else {
+        usleep(ISR_duration*1000);
+    }
     fprintf(execution_file, "%d, %d, END_IO\n", *current_time, ISR_duration);
-
     *current_time += ISR_duration;
 }
 
 
 //checks interrupt pritority
 void check_priority_of_ISR(int *current_time, FILE *execution_file){
+    usleep(1*1000);
     fprintf(execution_file, "%d, %d, check priority of interrupt\n", *current_time, 1);
     *current_time += 1; 
 }
 
 //check if ISR is masked
 void check_if_masked(int *current_time, FILE *execution_file){
+    usleep(1*1000);
     fprintf(execution_file, "%d, %d, check if masked\n", *current_time, 1);
     *current_time += 1; 
 }
@@ -80,12 +96,17 @@ void check_if_masked(int *current_time, FILE *execution_file){
 
 //represetns efforts by the cpu, duration obtained from trace.txt
 void cpu_execution(int *current_time, int duration, FILE *execution_file){
+    if (duration > 1000){
+        sleep(duration/1000);
+    } else {
+        usleep(duration*1000);
+    }
     fprintf(execution_file, "%d, %d, CPU execution\n", *current_time, duration);
     *current_time += duration;
 }
 
 int main(int argc, char *argv[]){
-    if (argc > 3){
+    if (argc > 4){
         printf("Too many command line inputs");
     }
 
@@ -130,7 +151,8 @@ int main(int argc, char *argv[]){
 
 
     //Let's prepare the output file
-    execution_file = fopen("execution.txt", "w");
+    char *execution_file_name = argv[3];
+    execution_file = fopen(execution_file_name, "w");
     if (execution_file == NULL) {
         printf("Error opening file.\n");
         return 1;  //If file couldn't open
